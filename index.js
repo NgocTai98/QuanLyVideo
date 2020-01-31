63982c2bf684800725b2d89d966ff7156b1ad46a
NgocTai <ngoctai1908@gmail.com>
2020-01-31 18:07:20 +0700

    connect to sql

--- index.js
+++ index.js
@@ -3,8 +3,38 @@ var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views","./views");
-app.listen(3000)
+app.listen(3000);
+
+var mysql = require('mysql');
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'QuanLyVideo'
+});
+
+connection.connect(function (error) {
+    if (!!error) {
+        console.log("Error");
+    }
+    else {
+        console.log("connected");
+    }
+});
+
+
 
 app.get('/', function (req, res) {
+    connection.query('SELECT * FROM video', function (error, rows, fields) {
+        if (!!error) {
+            console.log("Error in query");
+        }
+        else {
+            console.log("successful query");
+            console.log(rows);
+        }  
+      });
     res.render("home");
 })
